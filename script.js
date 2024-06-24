ed3292e32a89d50cb2c7f11d537331f7d14301f7
unknown <de-la-hogue.julien@orange.fr>
2024-06-24 13:04:12 +0200

    add supp button and local storage save

--- script.js
+++ script.js
@@ -11,7 +11,14 @@ function saveNote() {
     var title = document.getElementById('note_title').value
     var ul = document.getElementById("list");
     var li = document.createElement("li");
+    var texte = document.getElementById("notes").value;
     li.classList.add("note");
     li.appendChild(document.createTextNode(title + " " + currentDate));
     ul.appendChild(li);
+    window.localStorage.setItem(title, texte);
+}
+
+function deleteStorage(){
+    localStorage.clear();
+    location.reload();
 }
