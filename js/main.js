3f1362cf4953fdede26a2dccda7615b0bda2d3bc
Matteo <matteogallardo69@gmail.com>
2024-06-24 12:57:06 +0200

    Ajout de la branche

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const noteForm = document.getElementById("editor");
+  const noteTitleInput = document.getElementById("note_title");
+  const noteContentTextarea = document.getElementById("note_content");
+  const saveNoteBtn = document.getElementById("save_note_btn");
+  const notesList = document.getElementById("notesList");
+});
